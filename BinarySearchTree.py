87f711fb9852d95c6f0ecf0a731ae3d0381e21ed
Anirudh Ajith <anirudh.ajith@gmail.com>
2019-05-19 11:01:15 +0530

    Progress in BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,18 +1,14 @@
 from BinaryTree import BinaryTree
 
 class BinarySearchTree(BinaryTree):
-    
-    def __init__(self, key):
 
-        if key == None:
-            raise ValueError(self.__class__.__name__ + ' cannot have None key')
-        else:
-            BinaryTree.__init__(self, [key])
-    
+    def __init__(self, key):
+        BinaryTree.__init__(self, [key])
+
     def insert(self, key):
         currentKey = self.key[0]
 
-        if ckey == currentKey:
+        if key == currentKey:
             self.key.append(key)
         elif key < currentKey:
             if self.left == None:
@@ -28,7 +24,7 @@ class BinarySearchTree(BinaryTree):
                 self.right.insert(key)
         else:
             raise ValueError('Comparison failed')
-    
+
     def find(self, key):
         currentKey = self.key[0]
 
@@ -46,14 +42,14 @@ class BinarySearchTree(BinaryTree):
                 return self.right.find(key)
         else:
             raise ValueError('Comparison failed')
-    
+
     def count(self, key):
         reference = self.find(key)
         if reference == False:
             return 0
         else:
             return len(self.key)
-    
+
     def contains(self, key):
         count = self.count(key)
 
@@ -61,7 +57,7 @@ class BinarySearchTree(BinaryTree):
             return True
         else:
             return False
-    
+
     def delete(self, key):
         reference = self.find(key)
 
@@ -69,8 +65,37 @@ class BinarySearchTree(BinaryTree):
             return False
         else:
             reference._delete()
-            return True            
+            return True
+
+    def rotate(self, direction):
+        pass
     
+    def getMinimum(self):
+        if self.left == None:
+            return self.key[0]
+        else:
+            return self.left.getMinimum()
+    
+    def getMaximum(self):
+        if self.right == None:
+            return self.key[0]
+        else:
+            return self.right.getMaximum()
+    
+    def getSuccessor(self):
+        direction = self.getDirection()
+        if direction == 'right' or direction == None:
+            if self.right == None:
+                return None
+            else:
+                return self.right.getMinimum()
+        elif direction == 'left':
+            if self.right == None:
+                return self.parent
+            else:
+                return self.right.getMinimum()
+
+
     def _delete(self):
 
         if len(self.key) > 1:
@@ -96,16 +121,25 @@ class BinarySearchTree(BinaryTree):
                     self.right.parent = None
                     self.parent = None
                     self.right = None
-
         
-    
 
 
 
 
 '''
-TODO: insert DONE
-TODO: delete DONE
-TODO: balance
-TODO: find DONE
+Required Functionality:
+
+__init__(key): returns binary search tree initialized to key
+__init__(list): returns binary search tree initialized with an array of elements
+find(key): returns node containing key
+count(key): returns count of key in BST
+contains(key): Checks if BST contains key
+findSuccessor(key): returns successor node
+findPredecessor(key): returns predecessor node
+getSuccessor(key): returns successor node key
+getPredecessor(key): returns predecessor node key
+rotate(node): rotates
+insert(key): inserts key into BST
+delete(key): deletes key from BST
+
 '''
